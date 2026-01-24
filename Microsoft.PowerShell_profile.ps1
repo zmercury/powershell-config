7920eb0baaa86c409793ae4817f554b0691876fe
zmercury <messiniki@outlook.com>
2026-01-24 18:43:21 +0545

    feat: added repgrep with preview and open in nvim

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,8 @@
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 oh-my-posh init pwsh --eval --config "$PSScriptRoot\mercury.omp.json" | Invoke-Expression
+
 Clear-Host
+
 # fzf search and open in nvim
 function fzf-nvim {
     $file = rg --files |
@@ -16,5 +18,26 @@ function fzf-nvim {
 
 Set-PSReadLineKeyHandler -Key Ctrl+f -ScriptBlock {
     fzf-nvim
+}
+
+# Live ripgrep and open in nvim
+function fzf-rg-nvim {
+    $result = rg --line-number --no-heading --color=never "" |
+        fzf --delimiter : --preview "bat --style=numbers --color=always --highlight-line {2} {1}" |
+        Out-String
+
+    $result = $result.Trim()
+
+    if ($result) {
+        $parts = $result -split ":"
+        $file = $parts[0]
+        $line = $parts[1]
+
+        Start-Process nvim -ArgumentList "+$line `"$file`"" -NoNewWindow -Wait
+    }
+}
+
+Set-PSReadLineKeyHandler -Key Ctrl+x -ScriptBlock {
+    fzf-rg-nvim
 }
 
