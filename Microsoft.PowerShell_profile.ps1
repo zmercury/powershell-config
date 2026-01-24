9c288808fc7c4cf5d85e1d25344dd280ec8d4912
zmercury <messiniki@outlook.com>
2026-01-24 18:42:51 +0545

    feat: fzf find file and open in nvim

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,20 @@
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 oh-my-posh init pwsh --eval --config "$PSScriptRoot\mercury.omp.json" | Invoke-Expression
+Clear-Host
+# fzf search and open in nvim
+function fzf-nvim {
+    $file = rg --files |
+        fzf --preview "bat --style=numbers --color=always {}" |
+        Out-String
+
+    $file = $file.Trim()
+
+    if ($file) {
+        Start-Process nvim -ArgumentList "-- `"$file`"" -NoNewWindow -Wait
+    }
+}
+
+Set-PSReadLineKeyHandler -Key Ctrl+f -ScriptBlock {
+    fzf-nvim
+}
+
